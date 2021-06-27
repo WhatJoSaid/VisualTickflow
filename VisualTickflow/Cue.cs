c06785fd92dd95e7ccc5477cf2325a37fbb5bbfc
pixlblade <79928968+pixlblade@users.noreply.github.com>
2021-06-27 14:25:26 -0400

    sum work done

--- VisualTickflow/Cue.cs
+++ VisualTickflow/Cue.cs
@@ -1,13 +1,59 @@
-﻿namespace VisualTickflow
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisualTickflow
 {
+    // hoo boy messy code time
     public class Cue
     {
+        private string cue { get; set; }
+        private List<string> parameters { get; set; }
+        private float beat { get; set; }
         public Cue(string game, string cue, string[] parameters, float beat)
         {
+            this.beat = beat;
+            // nested switch statements? how??!?!??!
             switch (game)
             {
                 case "spaceball":
-                    
+                    switch (cue)
+                    {
+                        case "ball":
+                            if (MiscStuff.CheckLen(parameters, 2))
+                            {
+                                Console.WriteLine("Invalid Parameters!");
+                                break;
+                            }
+                            this.cue = "0x100";
+                            if (parameters[0] == "short")
+                            {
+                                this.parameters.Add("0x30");
+                            } else if (parameters[0] == "long")
+                            {
+                                this.parameters.Add("0x60");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid Parameters!");
+                                break;
+                            }
+                            switch (parameters[1])
+                            {
+                                case "ball":
+                                    this.parameters.Add("0");
+                                    break;
+                                case "rice":
+                                    this.parameters.Add("1");
+                                    break;
+                                case "alien":
+                                    this.parameters.Add("2");
+                                    break;
+                                default:
+                                    Console.WriteLine("Invalid Parameters!");
+                                    break;
+                            }
+                            break;
+                    }
                     break;
             }
         }
---
+++ VisualTickflow/MiscStuff.cs
@@ -0,0 +1,17 @@
+﻿namespace VisualTickflow
+{
+    public static class MiscStuff
+    {
+        public static bool CheckLen(string[] list, int numcheck)
+        {
+            if (list.Length == numcheck)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
