0aed22cdb59c978ea7c70f9fba02df984b70d990
pixlblade <79928968+pixlblade@users.noreply.github.com>
2021-06-27 22:07:58 -0400

    gone.

--- src/cues/spaceball.py
+++
@@ -1,6 +0,0 @@
-from src.tickflow import Cue
-
-class BallOutCue(Cue):
-    def __init__(self, beat, params):
-        self.cue = "0x100"
-        super().__init__(beat, params)
--- src/tickflow.py
+++
@@ -1,4 +0,0 @@
-class Cue:
-    def __init__(self, beat, params):
-        self.beat = beat
-        self.params = params
